2c1fb1fcdebeeee5e5278f9cd02468989d9dd113
Jason Eveleth <jj11@eveleth.com>
2024-01-24 05:51:20 -0500

    better build

--- build.zig
+++ build.zig
@@ -4,12 +4,19 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const exe = b.addExecutable(.{ .name = "jclj", .root_source_file = .{ .path = "src/main.zig" }, .optimize = optimize, .target = target });
+    const exe = b.addExecutable(.{
+        .name = "jclj",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .optimize = optimize,
+        .target = target,
+    });
     exe.addAssemblyFile(.{ .path = "src/vm.s" });
+    b.installArtifact(exe); // make `zig build` (which runs `install` by default) to move it to `zig-out`
 
     // setup `zig build run`
     const run_step = b.step("run", "Run the app");
     const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep()); // force `zig build run` to also move it to `zig-out`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
